a5e7b060b46571e098adc0328c8e408f8283d45a
sbsteacher <id@gmail.com>
2025-07-08 18:01:54 +0900

    Home.vue 작업 중

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -1,19 +1,18 @@
-import axios from 'axios';
-axios.defaults.baseURL = '/api/v1/account';
+import axios from './httpRequester';
+
 
 export const join = args => {
-    return axios.post('/join', args).catch(e => e.response);
+    return axios.post('/account/join', args).catch(e => e.response);
 }
 
 export const login = args => {
-    return axios.post('/login', args).catch(e => e.response);
+    return axios.post('/account/login', args).catch(e => e.response);
 }
 
 export const check = () => {
-    return axios.get('/check').catch(e => e.response);
+    return axios.get('/account/check').catch(e => e.response);
 }
 
 export const logout = () => {
-    return axios.post('/logout').catch(e => e.response);
-
+    return axios.post('/account/logout').catch(e => e.response);
 }
