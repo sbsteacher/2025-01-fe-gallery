00d89af29df296eaf059020ed3dd7d034f8b499a
sbsteacher <id@gmail.com>
2025-07-11 18:11:14 +0900

    주문 디테일 작업 중(2)

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -6,98 +6,106 @@ import { getOrder } from '@/services/orderService';
 const route = useRoute();
 
 const state = reactive({
-    order: {
-        id: 0,
-        name: '',
-        address: '',
-        payment: '',
-        amount: 0,
-        created: '',
-        items: []
-    }
+  order: {
+    id: 0,
+    name: '',
+    address: '',
+    payment: '',
+    amount: 0,
+    created: '',
+    items: [],
+  },
 });
 
 onMounted(async () => {
-    const orderId = route.params.id;
-    const res = await getOrder( orderId );
-    if(res === undefined || res.status !== 200) {
-        alert('오류 발생');
-        return;
-    }
-    state.order = res.data;
+  const orderId = route.params.id;
+  const res = await getOrder(orderId);
+  if (res === undefined || res.status !== 200) {
+    alert('오류 발생');
+    return;
+  }
+  state.order = res.data;
 });
-
 </script>
 
 <template>
-<div class="order-detail py-5">
+  <div class="order-detail py-5">
     <div class="container">
-        <div class="row">
-            <div class="order col-lg-8">
-                <div class="h5 mb-4">
-                    <b>주문 내용</b>
-                </div>
-                <table class="table table-bordered">
-                    <tbody>
-                        <tr>
-                            <th>주문 ID</th>
-                            <td>{{ state.order.id }}</td>
-                        </tr>
-                        <tr>
-                            <th>주문자명</th>
-                            <td>{{ state.order.name }}</td>
-                        </tr>
-                        <tr>
-                            <th>주소</th>
-                            <td>{{ state.order.address }}</td>
-                        </tr>
-                        <tr>
-                            <th>결제 금액</th>
-                            <td>{{ state.order.amount.toLocaleString() }}원</td>
-                        </tr>
-                        <tr>
-                            <th>결제 수단</th>
-                            <td>{{ state.order.payment === 'card' ? '카드' : '무통장입금' }}</td>
-                        </tr>
-                        <tr>
-                            <th>결제 일시</th>
-                            <td>{{ state.order.created.toLocaleString() }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <div class="items col-lg-4">
-                <div class="h5 mb-4">
-                    <b>주문 상품</b>
-                </div>
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>번호</th>
-                            <th>상품명</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(item, idx) in state.order.items" :key="item.id">
-                            <td>{{ idx + 1 }}</td>
-                            <td>{{ item.name }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>        
+      <div class="row">
+        <div class="order col-lg-8">
+          <div class="h5 mb-4">
+            <b>주문 내용</b>
+          </div>
+          <table class="table table-bordered">
+            <tbody>
+              <tr>
+                <th>주문 ID</th>
+                <td>{{ state.order.id }}</td>
+              </tr>
+              <tr>
+                <th>주문자명</th>
+                <td>{{ state.order.name }}</td>
+              </tr>
+              <tr>
+                <th>주소</th>
+                <td>{{ state.order.address }}</td>
+              </tr>
+              <tr>
+                <th>결제 금액</th>
+                <td>{{ state.order.amount.toLocaleString() }}원</td>
+              </tr>
+              <tr>
+                <th>결제 수단</th>
+                <td>
+                  {{ state.order.payment === 'card' ? '카드' : '무통장입금' }}
+                </td>
+              </tr>
+              <tr>
+                <th>결제 일시</th>
+                <td>{{ state.order.created.toLocaleString() }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+
+        <div class="items col-lg-4">
+          <div class="h5 mb-4">
+            <b>주문 상품</b>
+          </div>
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>번호</th>
+                <th>이미지</th>
+                <th>상품명</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, idx) in state.order.items" :key="item.id">
+                <td>{{ idx + 1 }}</td>
+                <td><img :src="`/pic/item/${item.imgPath}`" alt="테스트" /></td>
+                <td>{{ item.name }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .order-detail {
-    > .container {
-        table {
-            th, td { padding: 15px 25px; }
-            th { background-color: #f7f7f7; }
-        }
+  > .container {
+    table {
+      th,
+      td {
+        padding: 15px 25px;
+      }
+      th {
+        background-color: #f7f7f7;
+      }
     }
+  }
 }
 </style>
