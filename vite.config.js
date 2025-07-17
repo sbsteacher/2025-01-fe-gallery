f67ebe8cbb526ba2c3f0a64225b7a954dcb9707c
sbsteacher <id@gmail.com>
2025-07-17 17:34:13 +0900

    __APP__ENV 제거

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    __APP_ENV__: env.VITE_APP_ENV
-  },
+  ,
   base: process.env.NODE_ENV === 'production' ? '/2025-01-fe-gallery/' : '/',
   server: {
     proxy: {
