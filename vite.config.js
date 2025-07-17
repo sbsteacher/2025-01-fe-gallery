75ad9d15c8babdd8440f766dadf2a005c93fcf6f
sbsteacher <id@gmail.com>
2025-07-17 17:35:44 +0900

    콤마 실수

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  ,
   base: process.env.NODE_ENV === 'production' ? '/2025-01-fe-gallery/' : '/',
   server: {
     proxy: {
