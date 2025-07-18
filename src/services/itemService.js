adfce3fe493c2dcf5f3e8226d0bb1a4a46912447
sbsteacher <id@gmail.com>
2025-07-18 11:17:47 +0900

    상품 등록 작업 중 (2)

--- src/services/itemService.js
+++ src/services/itemService.js
@@ -1,5 +1,14 @@
 import axios from './httpRequester';
 
+export const addProduct = formData => {
+    const config = {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    }
+    return axios.post('/item', formData, config).catch(e => e.response);
+}
+
 export const getItems = () => {
     return axios.get('/item').catch(e => e.response);
 }
--- src/views/ProductForm.vue
+++ src/views/ProductForm.vue
@@ -1,12 +1,32 @@
 <script setup>
 import { reactive } from 'vue';
 import { useRouter } from 'vue-router';
+import { addProduct } from '@/services/itemService';
 
 const state = reactive({
-    form: {
-        name: ''
-    }
+    data: {
+        name: '',
+        price: 0,
+        discountPer: 0        
+    },
+    img: null
 });
+
+//파일 선택시 
+const handleFileSelected = e => {
+    const file = e.target.files[0];
+    console.log('file:', file);
+    state.img = file;
+}
+
+const submit = async () => {
+    const formData = new FormData();
+    formData.append('img', state.img);
+    formData.append('data', new Blob([JSON.stringify(state.data)]));
+
+    const res = await addProduct(formData);
+    console.log('res:', res);
+}
 
 </script>
 
@@ -27,25 +47,31 @@ const state = reactive({
                 <div class="row g-3">
                     <div class="col-12">
                         <label for="name" class="form-label">상품명</label>
-                        <input type="text" class="form-control p-3" id="name" v-model="state.form.name">
+                        <input type="text" class="form-control p-3" id="name" v-model="state.data.name">
                     </div>
 
                     <div class="col-12">
                         <label for="price" class="form-label">가격</label>
-                        <input type="number" class="form-control p-3" id="price" v-model="state.form.price">
+                        <input type="number" class="form-control p-3" id="price" v-model="state.data.price">
                     </div>
 
                     <div class="col-12">
                         <label for="discount" class="form-label">할인율</label>
-                        <input type="number" class="form-control p-3" id="discount" v-model="state.form.discountPer">
+                        <input type="number" class="form-control p-3" id="discount" v-model="state.data.discountPer">
                     </div>
 
                     <div class="col-12 pt-1">
-                        <label for="img" class="form-label">이미지</label>
-                        <input type="file" hidden class="form-control p-3">
-                        <button class="w-100 btn btn-primary py-4 mt-4">파일 선택</button>
+                        <label class="form-label">이미지</label>
+                        <input type="file" id="img" hidden class="form-control p-3" accept="image/*" 
+                                @change="handleFileSelected">
+                        <div>
+                            <label for="img">파일 선택</label>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div>            
+                <button class="w-100 btn btn-primary py-4 mt-4">등록</button>
             </div>
         </div>
     </form>
